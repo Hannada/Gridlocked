95f6efb573ebea5a16f46718aebc2b6475ca1dc7
Hannada <dphanna@gmail.com>
2019-10-17 15:08:51 -0400

    Moving sprite but not loaded through bundle

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import Game from "./scripts/player";
-import Player from "./scripts/player";
+// import Game from "./scripts/player";
+// import Player from "./scripts/player";
+import "./styles/index.scss";
+// import axios from "axios";
+
+// window.addEventListener("DOMContentLoaded", () => {
+//     document.getElementById("app").innerText = "Hello World!";
+//     axios.get("./search?").then(res => console.log(res));
+// });
+
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -8,8 +16,19 @@ document.addEventListener('DOMContentLoaded', () => {
     canvas.height = 480;
     const ctx = canvas.getContext("2d");
 
-    const play = () => {
-        const game = new Player(ctx)
-        game.startAnimating();
+    const background = new Image();
+    background.src = "../assets/images/image79.jpg"
+
+    background.onload = function () {
+        ctx.drawImage(background, 0, 0);
     }
+
+
+    // axios.get("./search?").then(res => console.log(res));
+
+
+    // const play = () => {
+    //     const game = new Player(ctx)
+    //     game.startAnimating();
+    // }
 });
