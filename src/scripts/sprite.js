ae5f1903c7f9d5779599c7548e15a1c60bfde098
Hannada <dphanna@gmail.com>
2020-06-03 16:44:45 -0400

    Added scaling note

--- src/scripts/sprite.js
+++ src/scripts/sprite.js
@@ -8,9 +8,12 @@ class Sprite {
     // Spite model should render sprite. Potentially carry over any other logic
     // Currently it should only draw sprite but might alter later.
 
-    // renderSprite( sx, sy, sw, sh, dx, dy, dw, dh, ctx) {
-    //     ctx.drawImage(this.image, sx, sy, sw, sh, dx, dy, dw, dh);
-    // }
+    // Multiplying the dwidth and dheight alters the scale of the sprite 
+    // Example in previous exercise on index
+
+    renderSprite( sx, sy, sw, sh, dx, dy, dw, dh, ctx) {
+        ctx.drawImage(this.image, sx, sy, sw, sh, dx, dy, dw, dh);
+    }
 }
 
 export default Sprite;
