165c9e93d8d2296f1d75d6aa64cb1ed90fec9621
Hannada <dphanna@gmail.com>
2020-06-03 16:38:47 -0400

    Woking on sprite model

--- src/scripts/sprite.js
+++ src/scripts/sprite.js
@@ -1,7 +1,13 @@
+import { text } from "express";
+
 class Sprite {
     constructor(img) {
         this.image = image;
     }
+
+    // renderSprite( sx, sy, sw, sh, dx, dy, dw, dh, ctx) {
+    //     ctx.drawImage(this.image, sx, sy, sw, sh, dx, dy, dw, dh);
+    // }
 }
 
 export default Sprite;
