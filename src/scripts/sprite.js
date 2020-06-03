75b14cc5d9837d34d022534b5fdc9aab03f08f69
Hannada <dphanna@gmail.com>
2020-06-03 16:39:37 -0400

    Added note

--- src/scripts/sprite.js
+++ src/scripts/sprite.js
@@ -5,6 +5,9 @@ class Sprite {
         this.image = image;
     }
 
+    // Spite model should render sprite. Potentially carry over any other logic
+    // Currently it should only draw sprite but might alter later.
+
     // renderSprite( sx, sy, sw, sh, dx, dy, dw, dh, ctx) {
     //     ctx.drawImage(this.image, sx, sy, sw, sh, dx, dy, dw, dh);
     // }
