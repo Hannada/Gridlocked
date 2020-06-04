59c42cc0880dcf335d8d43dd24deac0eb39619e4
Hannada <dphanna@gmail.com>
2020-06-04 00:13:14 -0400

    nightly

--- src/scripts/sprite.js
+++ src/scripts/sprite.js
@@ -11,6 +11,8 @@ class Sprite {
     // Multiplying the dwidth and dheight alters the scale of the sprite 
     // Example in previous exercise on index
 
+    // scale * dw and scale * dh 
+
     renderSprite( sx, sy, sw, sh, dx, dy, dw, dh, ctx) {
         ctx.drawImage(this.image, sx, sy, sw, sh, dx, dy, dw, dh);
     }
