d10a37024acfbce7b6dd283353f4d1f666b54aec
Hannada <dphanna@gmail.com>
2020-06-06 20:07:19 -0400

    Working on render

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -58,7 +58,7 @@ export default class Player {
 
     render() {
         // This should call rendersprite
-        // this.sprite.renderSprite()
+        this.sprite.renderSprite(sx, sy, sw, sh, dx, dy, dh, ctx)
     }
 
     // Try incorparating sprite.renderSprite for multiple sources
