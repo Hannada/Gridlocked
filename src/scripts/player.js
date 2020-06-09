4f9efa946fbf045eee21d485a1bbba9359235a44
Hannada <dphanna@gmail.com>
2020-06-08 22:35:38 -0400

    Putting proper values

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -58,7 +58,7 @@ export default class Player {
 
     render() {
         // This should call rendersprite
-        this.sprite.renderSprite(sx, sy, sw, sh, dx, dy, dh, ctx)
+        this.sprite.renderSprite(sx, sy, this.SCALED_WIDTH, this.SCALED_HEIGHT, dx, dy, dh, ctx)
     }
 
     // Try incorparating sprite.renderSprite for multiple sources
